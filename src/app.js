6a9822ead032d61bf738edf5d7e39f39687917a6
neyneyalldayday <drfrankenstein123@gmail.com>
2021-04-11 12:28:15 -0500

    webpack

--- src/app.js
+++ src/app.js
@@ -1,1 +1,6 @@
 // install -> import -> use 
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const template = <p>brittany is beautifull</p>;
+ReactDOM.render(template, document.getElementById('app'));

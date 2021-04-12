31510109e78fcaa9bee27f7c5bfe19a09733cc63
neyneyalldayday <drfrankenstein123@gmail.com>
2021-04-12 13:14:58 -0500

    this corse is 4 years old everything is broken

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,13 @@ module: {
         loader: 'babel-loader',
         test: /\.js$/,
         exclude: /node_modules/
+    },{
+        test: /\.scss$/,
+        use: [
+            'style-loader',
+            'css-loader',
+            'sass-loader'
+        ]
     }]    
 },
 devtool: 'cheap-module-eval-source-map',

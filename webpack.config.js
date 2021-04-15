ca844a016858210d8695ee7158254c554a988a86
neyneyalldayday <drfrankenstein123@gmail.com>
2021-04-15 13:35:44 -0500

    man this old version is killing me

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     entry: './src/app.js',
     output: {
         path: path.join(__dirname, 'public'),
-        filename: 'bundle.js',
+        filename: 'bundle.js',       
 },
 module: {
     rules: [{
